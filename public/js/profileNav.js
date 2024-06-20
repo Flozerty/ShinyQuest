2660ff5ce770f866d377bab05004820680cef00c
Flozerty <floris.louerat@gmail.com>
2024-06-20 16:07:17 +0200

    change profile-nav display method

--- public/js/profileNav.js
+++ public/js/profileNav.js
@@ -1,25 +1,30 @@
 const main = document.querySelector("#main-wrapper");
 const profileNav = document.querySelector('#profile-nav');
 const userContainer = document.querySelector('#header-profile');
-const caret = document.querySelector("#profile-caret")
+const caret = document.querySelector("#profile-caret");
 
 show = false;
 
-userContainer.addEventListener('click', () => {
-  if (show) {
-    caret.style.transform = "rotate(0deg)";
-    profileNav.style.transform = "translateY(-100%)";
-  } else {
-    caret.style.transform = "rotate(-90deg)";
-    profileNav.style.transform = "translateY(100%)";
-  }
-  show = !show;
-})
+displayers = [caret, userContainer];
+
+displayers.forEach(element => {
+
+  element.addEventListener('click', () => {
+    if (show) {
+      caret.style.transform = "rotate(-90deg)";
+      profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
+    } else {
+      caret.style.transform = "rotate(0deg)";
+      profileNav.style.transform = "translate(0, 100%)";
+    }
+    show = !show;
+  })
+});
 
 main.addEventListener("click", () => {
   if (show) {
-    caret.style.transform = "rotate(0deg)";
-    profileNav.style.transform = "translateY(-100%)";
+    caret.style.transform = "rotate(-90deg)";
+    profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
     show = !show;
   }
 })
