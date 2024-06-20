ff60729b7e1e0965cdce0a804defc3db529e7905
Flozerty <floris.louerat@gmail.com>
2024-06-20 16:13:58 +0200

    + withdraw profile-nav when click on header

--- public/js/profileNav.js
+++ public/js/profileNav.js
@@ -2,13 +2,14 @@ const main = document.querySelector("#main-wrapper");
 const profileNav = document.querySelector('#profile-nav');
 const userContainer = document.querySelector('#header-profile');
 const caret = document.querySelector("#profile-caret");
+const header = document.querySelector("header");
 
 show = false;
 
 displayers = [caret, userContainer];
 
+// montrer / cacher la profile-nav 
 displayers.forEach(element => {
-
   element.addEventListener('click', () => {
     if (show) {
       caret.style.transform = "rotate(-90deg)";
@@ -21,8 +22,18 @@ displayers.forEach(element => {
   })
 });
 
+// retire la nav quand on clique ailleurs
 main.addEventListener("click", () => {
   if (show) {
+    caret.style.transform = "rotate(-90deg)";
+    profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
+    show = !show;
+  }
+})
+
+// retire la nav quand on clique sur le header (sauf sur les displayers)
+header.addEventListener("click", (event) => {
+  if (show && !caret.contains(event.target) && !userContainer.contains(event.target)) {
     caret.style.transform = "rotate(-90deg)";
     profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
     show = !show;
