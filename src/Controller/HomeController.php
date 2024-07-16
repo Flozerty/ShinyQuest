505cd6ae6000e76d86420afc8fb917a23969ae96
Flozerty <floris.louerat@gmail.com>
2024-07-16 15:26:04 +0200

    homeController method

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CaptureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,17 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(CaptureRepository $captureRepository,): Response
     {
+        $user = $this->getUser();
+
+        $shasses = $captureRepository->findBy(['user' => $user, 'suivi' => 1]);
+
+        $captures = $captureRepository->findBy(['termine' => 1], ["dateCapture" => "DESC"], 10);
+
         return $this->render('home/index.html.twig', [
+            "shasses" => $shasses,
+            "captures" => $captures,
         ]);
     }
 }
