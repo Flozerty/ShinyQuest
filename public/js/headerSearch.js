ff386c815f39a94069efe297387806cbe683f1ac
Flozerty <floris.louerat@gmail.com>
2024-07-23 16:37:48 +0200

    add a timeout condition & a "diff from last request" condition before sending a new search request

--- public/js/headerSearch.js
+++ public/js/headerSearch.js
@@ -1,56 +1,70 @@
 $(document).ready(function () {
 
-    const searchInput = document.getElementById('search-input');
-    const searchResults = document.getElementById('search-results');
+  const searchInput = document.querySelector('#search-input');
+  const searchResults = document.querySelector('#search-results');
 
-    searchInput.addEventListener('input', () => {
-        const query = searchInput.value;
+  let lastQuery;
+  let timeout;
 
-        // conditions de réalisation de la requete
-        if (query.length > 2) {
+  searchInput.addEventListener('input', () => {
+    const query = searchInput.value;
+    clearTimeout(timeout);
 
-            fetch(`/search?q=${encodeURIComponent(query)}`)
-                .then(response => response.json())
-                .then(data => {
-                    displayResults(data);
-                });
-        } else {
-            searchResults.innerHTML = '';
-        }
-    });
-
-    function displayResults(data) {
-        // console.log(data)
-        searchResults.innerHTML = '';
-
-        if (data.pokemons.length > 0) {
-            const pkmnList = document.createElement('ul');
-            pkmnList.innerHTML = '<li class="search-title">Pokémons</li>';
-
-            data.pokemons.forEach(pokemon => {
-                const li = document.createElement('li');
-                li.classList.add('search-result')
-                li.innerHTML = "<a href='/pokemon/" + pokemon.pokedex_id + "'>" + pokemon.pokedex_id + " - " + pokemon.name + "</a>";
-                pkmnList.appendChild(li);
-            });
-            searchResults.appendChild(pkmnList);
-        }
-
-        if (data.users.length > 0) {
-            const userList = document.createElement('ul');
-            userList.innerHTML = '<li class="search-title">Dresseurs</li>';
-
-            data.users.forEach(user => {
-                const li = document.createElement('li');
-                li.classList.add('search-result')
-                li.innerHTML = "<a href='/users/" + user.id + "'>" + user.pseudo + "</a>";
-                userList.appendChild(li);
-            });
-            searchResults.appendChild(userList);
-        }
-
-        if (data.users.length == 0 && data.pokemons.length == 0) {
-            searchResults.innerHTML = "<p>Aucun résultat trouvé</p>";
-        }
+    // conditions de réalisation de la requete
+    if (query.length <= 2) {
+      searchResults.innerHTML = '';
+      return;
     }
+
+    // Si query.length > 2 && user ne tape plus au clavier depuis .5 seconde && query différente de la dernière envoyée
+    timeout = setTimeout(() => {
+      if (query !== lastQuery) {
+        // console.log('requête envoyée pour : ' + query)
+        fetch(`/search?q=${query}`)
+          .then(response => response.json())
+          .then(data => {
+            displayResults(data);
+            lastQuery = query;
+          })
+          .catch(error => {
+            console.error('Erreur en récupérant les résultats:', error);
+          });
+      }
+    }, 500);
+  });
+
+  function displayResults(data) {
+    // console.log(data)
+    searchResults.innerHTML = '';
+
+    if (data.pokemons.length > 0) {
+      const pkmnList = document.createElement('ul');
+      pkmnList.innerHTML = '<li class="search-title">Pokémons</li>';
+
+      data.pokemons.forEach(pokemon => {
+        const li = document.createElement('li');
+        li.classList.add('search-result')
+        li.innerHTML = `<a href='/pokemon/${pokemon.pokedex_id}'>${pokemon.pokedex_id} - ${pokemon.name}</a>`;
+        pkmnList.appendChild(li);
+      });
+      searchResults.appendChild(pkmnList);
+    }
+
+    if (data.users.length > 0) {
+      const userList = document.createElement('ul');
+      userList.innerHTML = '<li class="search-title">Dresseurs</li>';
+
+      data.users.forEach(user => {
+        const li = document.createElement('li');
+        li.classList.add('search-result')
+        li.innerHTML = "<a href='/users/" + user.id + "'>" + user.pseudo + "</a>";
+        userList.appendChild(li);
+      });
+      searchResults.appendChild(userList);
+    }
+
+    if (data.users.length == 0 && data.pokemons.length == 0) {
+      searchResults.innerHTML = "<p>Aucun résultat trouvé</p>";
+    }
+  }
 });
