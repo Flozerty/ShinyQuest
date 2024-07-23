a01c30d2a42eef0c4330521836afc8ef3793663c
Flozerty <floris.louerat@gmail.com>
2024-07-23 15:31:28 +0200

    add condition if "my profile"

--- public/js/profileModals.js
+++ public/js/profileModals.js
@@ -2,51 +2,56 @@ document.addEventListener('DOMContentLoaded', function () {
 
   const body = document.querySelector('body');
 
-  const avatarButton = document.querySelector("#change-avatar-button")
-  const pseudoButton = document.querySelector("#change-pseudo-button")
+  const editSection = document.querySelector("#edit-account")
 
-  const avatarDialog = document.querySelector("#avatarDialog")
-  const pseudoDialog = document.querySelector("#pseudoDialog")
+  if (editSection) {
+
+    const avatarButton = document.querySelector("#change-avatar-button")
+    const pseudoButton = document.querySelector("#change-pseudo-button")
+
+    const avatarDialog = document.querySelector("#avatarDialog")
+    const pseudoDialog = document.querySelector("#pseudoDialog")
 
 
-  avatarButton.addEventListener('click', () => {
-    showDialog(avatarDialog)
-    openCheck(avatarDialog)
-  })
+    avatarButton.addEventListener('click', () => {
+      showDialog(avatarDialog)
+      openCheck(avatarDialog)
+    })
 
-  pseudoButton.addEventListener('click', () => {
-    showDialog(pseudoDialog)
-    openCheck(pseudoDialog)
-  })
+    pseudoButton.addEventListener('click', () => {
+      showDialog(pseudoDialog)
+      openCheck(pseudoDialog)
+    })
 
-  // si on clique sur le backdrop de la modal, on veut fermer la modal
-  function showDialog(dialog) {
-    dialog.showModal();
-    const dialogContent = dialog.querySelector('.dialog-content');
-    const cancelButton = dialog.querySelector(".cancelBtn");
+    // si on clique sur le backdrop de la modal, on veut fermer la modal
+    function showDialog(dialog) {
+      dialog.showModal();
+      const dialogContent = dialog.querySelector('.dialog-content');
+      const cancelButton = dialog.querySelector(".cancelBtn");
 
-    cancelButton.addEventListener("click", () => {
+      cancelButton.addEventListener("click", () => {
 
+        if (dialog.open) {
+          dialog.close();
+          openCheck(dialog);
+        }
+      });
+
+      document.addEventListener('click', (event) => {
+        if (!dialogContent.contains(event.target) && dialog.contains(event.target)) {
+          dialog.close();
+          openCheck(dialog);
+        }
+      });
+    }
+
+    // check si le dialog est ouvert
+    function openCheck(dialog) {
       if (dialog.open) {
-        dialog.close();
-        openCheck(dialog);
+        body.style.filter = "blur(3px)";
+      } else {
+        body.style.filter = "";
       }
-    });
-
-    document.addEventListener('click', (event) => {
-      if (!dialogContent.contains(event.target) && dialog.contains(event.target)) {
-        dialog.close();
-        openCheck(dialog);
-      }
-    });
-  }
-
-  // check si le dialog est ouvert
-  function openCheck(dialog) {
-    if (dialog.open) {
-      body.style.filter = "blur(3px)";
-    } else {
-      body.style.filter = "";
     }
   }
 })
