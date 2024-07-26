8e56db1913687262c1f3eaf35537f31ec81d774e
Flozerty <floris.louerat@gmail.com>
2024-07-26 19:15:07 +0200

    pokemon stats script

---
+++ public/js/pokemonStats.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const statsComp = document.querySelectorAll('.stat-completion')
+
+    statsComp.forEach((element) => {
+        const stat = element.getAttribute('title').split('/')[0];
+        const percentage = (stat / 220) * 100;
+        const completionBar = element.querySelector('.completion-bar');
+
+        setTimeout(() => {
+            completionBar.style.width = percentage + '%';
+        }, 1000);
+    });
+});
