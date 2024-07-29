85c0cc0c74d202353072f62f7b7ea928a9a96944
Flozerty <floris.louerat@gmail.com>
2024-07-29 15:10:21 +0200

    change header profile nav : open on hover

--- public/js/headerProfileNav.js
+++ public/js/headerProfileNav.js
@@ -5,37 +5,46 @@ const caret = document.querySelector("#profile-caret");
 const header = document.querySelector("header");
 
 show = false;
-
 displayers = [caret, userContainer];
 
 // montrer / cacher la profile-nav 
 displayers.forEach(element => {
   element.addEventListener('click', () => {
     if (show) {
-      caret.style.transform = "rotate(-90deg)";
-      profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
+      hideProfileNav()
     } else {
-      caret.style.transform = "rotate(0deg)";
-      profileNav.style.transform = "translate(0, 100%)";
+      showProfileNav()
     }
-    show = !show;
   })
+  element.addEventListener('mouseenter', () => {
+    if (!show) {
+      showProfileNav()
+    }
+  });
 });
 
 // retire la nav quand on clique ailleurs
 main.addEventListener("click", () => {
   if (show) {
-    caret.style.transform = "rotate(-90deg)";
-    profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
-    show = !show;
+    hideProfileNav()
   }
 })
 
 // retire la nav quand on clique sur le header (sauf sur les displayers)
 header.addEventListener("click", (event) => {
   if (show && !caret.contains(event.target) && !userContainer.contains(event.target)) {
-    caret.style.transform = "rotate(-90deg)";
-    profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
-    show = !show;
+    hideProfileNav()
   }
 })
+
+function showProfileNav() {
+  caret.style.transform = "rotate(0deg)";
+  profileNav.style.transform = "translate(0, 100%)";
+  show = true;
+}
+
+function hideProfileNav() {
+  caret.style.transform = "rotate(-90deg)";
+  profileNav.style.transform = "translate(150%, 50%) rotate(45deg)";
+  show = false;
+}
