03bf8d32c09bd687567ad4e722a1d6e53074329a
Flozerty <floris.louerat@gmail.com>
2024-07-30 12:02:16 +0200

    + home if disconnected

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,17 +11,27 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     #[Route('/home', name: 'app_home')]
-    public function index(CaptureRepository $captureRepository, ): Response
+    public function index(CaptureRepository $captureRepository,): Response
     {
         $user = $this->getUser();
-        $shasses = $captureRepository->findBy(['user' => $user, 'suivi' => 1]);
 
-        $captures = $captureRepository->findBy(['termine' => 1], ["dateCapture" => "DESC"], 10);
+        if ($user) {
+            $shasses = $captureRepository->findBy(['user' => $user, 'suivi' => 1]);
 
-        return $this->render('home/index.html.twig', [
-            "page_title" => "Bienvenue sur SHINYQUEST",
-            "shasses" => $shasses,
-            "captures" => $captures,
-        ]);
+            $captures = $captureRepository->findBy(['termine' => 1], ["dateCapture" => "DESC"], 10);
+
+            return $this->render('home/index.html.twig', [
+                "page_title" => "Bienvenue sur SHINYQUEST",
+                "shasses" => $shasses,
+                "captures" => $captures,
+            ]);
+        } else {
+            $captures = $captureRepository->findBy(['termine' => 1], ["dateCapture" => "DESC"], 10);
+
+            return $this->render('home/homeDisconnected.html.twig', [
+                "page_title" => "Bienvenue sur SHINYQUEST",
+                "captures" => $captures,
+            ]);
+        }
     }
 }
