22160093996f4884bc6e60d2ebe43ee5254d762b
flozerty <floris.louerat@gmail.com>
2024-07-31 11:02:13 +0200

    cleaning some code

--- public/js/base.js
+++ public/js/base.js
@@ -4,13 +4,7 @@ const footer = document.querySelector('footer'),
 
 function adjustMainHeight() {
 	mainWrapper.style.minHeight = `calc(100vh - ${footer.offsetHeight}px)`
-	console.log(mainWrapper.style.minHeight)
+	// console.log(mainWrapper.style.minHeight)
 }
-adjustMainHeight();
 
-document.addEventListener("click", event => {
-	// l/d mode
-	if (event.target.name == "ldMode") {
-		event.target.removeAttribute("id");
-	}
-});
+adjustMainHeight();
--- public/js/toggleSideNav.js
+++ public/js/toggleSideNav.js
@@ -1,6 +1,6 @@
 const sideNavContainer = document.querySelector('#main-side-nav'),
   toggleBtn = document.querySelector('#toggle-nav-btn'),
-  mainContainer = document.querySelector('#main-wrapper')
+  mainContainer = document.querySelector('main')
 
 let toggle = false;
 
@@ -15,10 +15,12 @@ function verifyToggle() {
     toggleBtn.style.transform = "translateX(60%) rotate(180deg)";
     sideNavContainer.classList.add('toggle-nav');
     sideNavContainer.classList.remove('hide-nav');
+    mainContainer.classList.add("main-blur")
 
   } else {
     toggleBtn.style.transform = "translateX(60%)";
     sideNavContainer.classList.add('hide-nav');
     sideNavContainer.classList.remove('toggle-nav');
+    mainContainer.style.filter = ""
   }
 }
