02a4cacd032a51b79a8a2ea919a3588c1bf7da72
flozerty <floris.louerat@gmail.com>
2024-08-01 11:51:49 +0200

    responsive amis & profile pages

---
+++ public/js/amisToggleLinks.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+  const amisLinksContainer = document.querySelectorAll('.menu-burger-ami')
+
+  amisLinksContainer.forEach(container => {
+    const displayButton = container.querySelector('.toggle-menu'),
+      linksDiv = container.querySelector('ul');
+
+    let toggle = false;
+
+    displayButton.addEventListener('click', () => {
+      toggle = !toggle;
+      verifyToggle();
+    })
+
+    verifyToggle()
+
+    function verifyToggle() {
+      if (toggle) {
+        linksDiv.classList.remove('hide')
+      } else {
+        linksDiv.classList.add('hide')
+      }
+    }
+  })
+})
