f75d5e7a7c090dbfab636a7c1e6f7100d0ce2f39
Flozerty <floris.louerat@gmail.com>
2024-08-24 16:13:30 +0200

    add some colors and fix page load flash

--- public/js/setColors.js
+++ public/js/setColors.js
@@ -1,93 +1,110 @@
 document.addEventListener('DOMContentLoaded', function () {
+  const colorSelection = document.querySelectorAll('.l_d_mode-main input, .l_d_mode input');
 
-    // initailisation aux paramètres enregistrés
-    loadSavedSelection();
+  colorSelection.forEach(choice => {
+    choice.addEventListener('change', function () {
+      syncSelection(this.id);
+      changeSelectedColor(this.id);
+      saveSelection(this.id)
+    })
+  });
 
-    const colorSelection = document.querySelectorAll('.l_d_mode-main input, .l_d_mode input');
+  function syncSelection(selectedId) {
+    // On s'assure que les 2 sélecteurs se mettent à jour
+    if (selectedId === 'pokeball-toggle' || selectedId === 'pokeball-toggle2') {
+      document.getElementById('pokeball-toggle').checked = true;
+      document.getElementById('pokeball-toggle2').checked = true;
 
-    colorSelection.forEach(choice => {
-        choice.addEventListener('change', function () {
-            syncSelection(this.id);
-            changeSelectedColor(this.id);
-            saveSelection(this.id)
-        });
-    });
+    } else if (selectedId === 'superball-toggle' || selectedId === 'superball-toggle2') {
+      document.getElementById('superball-toggle').checked = true;
+      document.getElementById('superball-toggle2').checked = true;
 
-    function syncSelection(selectedId) {
-        // On s'assure que les 2 sélecteurs se mettent à jour
-        if (selectedId === 'pokeball-toggle' || selectedId === 'pokeball-toggle2') {
-            document.getElementById('pokeball-toggle').checked = true;
-            document.getElementById('pokeball-toggle2').checked = true;
-
-        } else if (selectedId === 'superball-toggle' || selectedId === 'superball-toggle2') {
-            document.getElementById('superball-toggle').checked = true;
-            document.getElementById('superball-toggle2').checked = true;
-
-        } else if (selectedId === 'hyperball-toggle' || selectedId === 'hyperball-toggle2') {
-            document.getElementById('hyperball-toggle').checked = true;
-            document.getElementById('hyperball-toggle2').checked = true;
-        }
+    } else if (selectedId === 'hyperball-toggle' || selectedId === 'hyperball-toggle2') {
+      document.getElementById('hyperball-toggle').checked = true;
+      document.getElementById('hyperball-toggle2').checked = true;
     }
+  }
 
-    function changeSelectedColor(selectedId) {
-        if (selectedId === 'pokeball-toggle' || selectedId === 'pokeball-toggle2') {
-            setColors(
-                '--darkred',
-                '--lightred',
-                '--background-light-main',
-                '--light-secondary',
-                '--black',
-                '--white',
-            );
-        } else if (selectedId === 'superball-toggle' || selectedId === 'superball-toggle2') {
-            setColors(
-                '--darkblue',
-                '--lightblue',
-                '--background-light-main',
-                '--light-secondary',
-                '--black',
-                '--white',
-            );
-        } else if (selectedId === 'hyperball-toggle' || selectedId === 'hyperball-toggle2') {
-            setColors(
-                '--yellow',
-                '--dark-secondary',
-                '--background-dark-main',
-                '--dark-secondary',
-                '--white',
-                '--dark-card-color',
-            );
-        }
-    }
-
-    // fonction qui mettra a jour les coulurs dans :root
-    function setColors(
-        primaryColor,
-        secondaryColor,
-        backgroundColor,
-        backgroundSecondaryColor,
-        fontMainColor,
-        cardColor,
-    ) {
-        document.documentElement.style.setProperty('--primary-color', `var(${primaryColor})`);
-        document.documentElement.style.setProperty('--secondary-color', `var(${secondaryColor})`);
-        document.documentElement.style.setProperty('--background-color', `var(${backgroundColor})`);
-        document.documentElement.style.setProperty('--background-secondary', `var(${backgroundSecondaryColor})`);
-        document.documentElement.style.setProperty('--font-main-color', `var(${fontMainColor})`);
-        document.documentElement.style.setProperty('--cardColor', `var(${cardColor})`);
-    }
-
-    // sauvegarde de la sélection dans le Local Storage
-    function saveSelection(selectedId) {
-        localStorage.setItem('colorSelection', selectedId);
-    }
-
-    function loadSavedSelection() {
-        const savedId = localStorage.getItem('colorSelection');
-        if (savedId) {
-            const input = document.getElementById(savedId);
-            input.checked = true;
-            changeSelectedColor(savedId);
-        }
-    }
+  // sauvegarde de la sélection dans le Local Storage
+  function saveSelection(selectedId) {
+    localStorage.setItem('colorSelection', selectedId);
+  }
 });
+
+
+///////////////////////////////////////////////////////////////////////
+/////////////////////////// INITIALISATION ////////////////////////////
+///////////////////////////////////////////////////////////////////////
+loadSavedSelection();
+
+function loadSavedSelection() {
+  const savedId = localStorage.getItem('colorSelection');
+  if (savedId) {
+    changeSelectedColor(savedId);
+
+    // on va changer l'input sélectionné dès le chargement de la page terminé
+    document.addEventListener('DOMContentLoaded', function () {
+      const input = document.getElementById(savedId);
+      input.checked = true;
+    })
+  }
+}
+
+// attribue les couleurs en fonction de l'input sélectionné
+function changeSelectedColor(selectedId) {
+  if (selectedId === 'pokeball-toggle' || selectedId === 'pokeball-toggle2') {
+    setColors(
+      '--darkred',
+      '--lightred',
+      '--background-light-main',
+      '--light-secondary',
+      '--black',
+      '--white',
+      '--light-secondary-full',
+      '--darkblue'
+    );
+  } else if (selectedId === 'superball-toggle' || selectedId === 'superball-toggle2') {
+    setColors(
+      '--darkblue',
+      '--lightblue',
+      '--background-light-main',
+      '--light-secondary',
+      '--black',
+      '--white',
+      '--light-secondary-full',
+      '--darkblue'
+    );
+  } else if (selectedId === 'hyperball-toggle' || selectedId === 'hyperball-toggle2') {
+    setColors(
+      '--yellow',
+      '--dark-secondary',
+      '--background-dark-main',
+      '--dark-secondary',
+      '--white',
+      '--dark-card-color',
+      '--dark-secondary-full',
+      '--lightblue'
+    );
+  }
+}
+
+// fonction qui met a jour les coulurs dans :root
+function setColors(
+  primaryColor,
+  secondaryColor,
+  backgroundColor,
+  backgroundSecondaryColor,
+  fontMainColor,
+  cardColor,
+  cardSecondaryColor,
+  blueMain,
+) {
+  document.documentElement.style.setProperty('--primary-color', `var(${primaryColor})`);
+  document.documentElement.style.setProperty('--secondary-color', `var(${secondaryColor})`);
+  document.documentElement.style.setProperty('--background-color', `var(${backgroundColor})`);
+  document.documentElement.style.setProperty('--background-secondary', `var(${backgroundSecondaryColor})`);
+  document.documentElement.style.setProperty('--font-main-color', `var(${fontMainColor})`);
+  document.documentElement.style.setProperty('--cardColor', `var(${cardColor})`);
+  document.documentElement.style.setProperty('--cardSecondaryColor', `var(${cardSecondaryColor})`);
+  document.documentElement.style.setProperty('--blue-main', `var(${blueMain})`);
+}
