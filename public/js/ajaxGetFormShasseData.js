b533f8367b16d77f42c1657a157b3348fb19010a
Flozerty <floris.louerat@gmail.com>
2024-08-31 23:48:47 +0200

    loading spinners in shasse ajax import data

--- public/js/ajaxGetFormShasseData.js
+++ public/js/ajaxGetFormShasseData.js
@@ -1,29 +1,66 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const pokemonsContainer = document.getElementById('pokemonId');
-    const gamesContainer = document.getElementById('jeu');
+  const pokemonsContainer = document.getElementById('pokemonId'),
+    gamesContainer = document.getElementById('jeu'),
+    ballSelect = document.getElementById('ball');
 
-    fetch('/api/pokemons')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(pokemon => {
-                const element = document.createElement('option');
-                // <option value="{{ pokemon.pokedex_id }}">{{ pokemon.pokedex_id }} - {{ pokemon.name }}</option>
-                element.value = pokemon.pokedex_id;
-                element.innerHTML = pokemon.pokedex_id + " - " + pokemon.name;
-                pokemonsContainer.appendChild(element);
-            });
-        })
-        .catch(error => console.error('Erreur lors du chargement des pokemons:', error));
+  // loading containers
+  const compteurLoading = "",
+    jeuLoading = document.querySelector('.loading.jeu-loading'),
+    nomPokemonLoading = document.querySelector('.loading.nom-pokemon-loading'),
+    ballLoading = document.querySelector('.loading.ball-loading');
 
-    fetch('/api/games')
-        .then(response => response.json())
-        .then(data => {
-            data.forEach(game => {
-                const element = document.createElement('option');
-                element.value = game;
-                element.innerHTML = game;
-                gamesContainer.appendChild(element);
-            });
-        })
-        .catch(error => console.error('Erreur lors du chargement des jeux:', error));
+  /////////////////// récupération des pokemons ///////////////////
+  fetch('/api/pokemons')
+    .then(response => response.json())
+    .then(data => {
+      nomPokemonLoading.remove();
+
+      data.forEach(pokemon => {
+        const element = document.createElement('option');
+        // <option value="{{ pokemon.pokedex_id }}">{{ pokemon.pokedex_id }} - {{ pokemon.name }}</option>
+        element.value = pokemon.pokedex_id;
+        element.innerHTML = pokemon.pokedex_id + " - " + pokemon.name;
+        pokemonsContainer.appendChild(element);
+      });
+    })
+
+  ///////////////////// récupération des jeux /////////////////////
+  fetch('/api/games')
+    .then(response => response.json())
+    .then(data => {
+      jeuLoading.remove();
+
+      data.forEach(game => {
+        const element = document.createElement('option');
+        element.value = game;
+        element.innerHTML = game;
+        gamesContainer.appendChild(element);
+      });
+    })
+
+  ///////////////////// récupération des balls /////////////////////
+  fetch('/api/balls')
+    .then(response => response.json())
+    .then(data => {
+      ballLoading.remove();
+
+      data.forEach(group => {
+        if (group && group.ballsData) {
+          const optgroup = document.createElement('optgroup');
+          optgroup.label = group.categoryName;
+
+          group.ballsData.forEach(ball => {
+            // gérer la condition si null
+            if (ball && ball.spriteName && ball.name) {
+              const option = document.createElement('option');
+              option.value = ball.spriteName;
+              option.textContent = ball.name;
+              optgroup.appendChild(option);
+            }
+          });
+
+          ballSelect.appendChild(optgroup);
+        }
+      });
+    })
 });
