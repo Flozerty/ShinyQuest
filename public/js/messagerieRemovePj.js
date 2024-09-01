855e39b528b57da78d0d4016a8993629539e1ee8
Flozerty <floris.louerat@gmail.com>
2024-09-01 15:07:46 +0200

    scroll to bottom conversation

--- public/js/messagerieRemovePj.js
+++ public/js/messagerieRemovePj.js
@@ -1,11 +1,25 @@
 document.addEventListener('DOMContentLoaded', function () {
   const inputContainer = document.querySelector('#pjContainer')
 
+  // remove PJ
   if (inputContainer) {
     const delBtn = inputContainer.querySelector('.fa-trash-can')
-
     delBtn.addEventListener('click', () => {
       inputContainer.remove()
     })
   }
-})
+
+  // initailisation de la page : scroll to bot
+  scrollToBot();
+
+  function scrollToBot() {
+    const footer = document.querySelector('footer');
+    if (footer) {
+      const footerTop = footer.getBoundingClientRect().top + window.scrollY; //+ window.scrollY pour quand on refresh la page dans la conv
+
+      window.scrollTo({
+        top: footerTop - window.innerHeight
+      });
+    }
+  }
+});
