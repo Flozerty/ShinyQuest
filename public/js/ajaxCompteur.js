5d25d9404b4a58e3a69002092bf2000359f2ccfe
Flozerty <floris.louerat@gmail.com>
2024-09-01 11:09:46 +0200

    more factorisation & interval conditions to requests & add loading spinner

--- public/js/ajaxCompteur.js
+++ public/js/ajaxCompteur.js
@@ -3,37 +3,40 @@ document.addEventListener('DOMContentLoaded', function () {
   const cards = document.querySelectorAll('.pokemon-card')
 
   cards.forEach(card => {
-    const incrementButton = card.querySelector('.increment');
-    const decrementButton = card.querySelector('.decrement');
-    const increment10Button = card.querySelector('.increment10');
-    const decrement10Button = card.querySelector('.decrement10');
-    const compteurInput = card.querySelector('.compteur-input');
-    const captureId = card.getAttribute('data-id');
+    const incrementButton = card.querySelector('.increment'),
+      decrementButton = card.querySelector('.decrement'),
+      increment10Button = card.querySelector('.increment10'),
+      decrement10Button = card.querySelector('.decrement10');
 
-    // increment
-    incrementButton.addEventListener('click', function () {
-      verifyToStartLoading()
-      compteurInput.value = parseInt(compteurInput.value) + 1
-      updateCounter(`increment`);
+    const formCompteur = card.querySelector('.form-like-compteur'),
+      compteurInput = card.querySelector('.compteur-input');
+
+    const captureId = card.getAttribute('data-id');
+    let timeout;
+
+    incrementButton.addEventListener('click', () => {
+      changeCounter(1)
     });
-    // decrement
-    decrementButton.addEventListener('click', function () {
-      verifyToStartLoading()
-      compteurInput.value = parseInt(compteurInput.value) - 1
-      updateCounter(`decrement`);
+    decrementButton.addEventListener('click', () => {
+      changeCounter(-1)
     });
-    // increment 10
-    increment10Button.addEventListener('click', function () {
-      verifyToStartLoading()
-      compteurInput.value = parseInt(compteurInput.value) + 10
-      updateCounter(`increment10`);
+    increment10Button.addEventListener('click', () => {
+      changeCounter(10)
     });
-    // decrement 10
-    decrement10Button.addEventListener('click', function () {
-      verifyToStartLoading()
-      compteurInput.value = parseInt(compteurInput.value) - 10
-      updateCounter(`decrement10`);
+    decrement10Button.addEventListener('click', () => {
+      changeCounter(-10)
     });
+
+    // change la valeur de l'input
+    function changeCounter(value) {
+      compteurInput.value = parseInt(compteurInput.value) + value;
+      verifyToUpdate();
+    }
+
+    // compteur input change
+    compteurInput.addEventListener('change', () => {
+      verifyToUpdate()
+    })
 
     function updateCounter() {
       const newValue = compteurInput.value;
@@ -41,23 +44,25 @@ document.addEventListener('DOMContentLoaded', function () {
       fetch(`/shasse/${captureId}/updateCounter/${newValue}`)
         .then(response => response.json())
         .then(data => {
+          formCompteur.querySelector('.input-loading').remove()
           console.log(data.nbRencontres)
-          verifyToRemoveLoading()
-          // console.log(data.nbRencontres)
         })
     }
 
-    // compteur input change
-    compteurInput.addEventListener('change', function () {
-      updateCounter()
-    })
+    function verifyToUpdate() {
+      if (timeout) {
+        clearTimeout(timeout);
+        spinner = formCompteur.querySelector('.input-loading');
+        spinner ? spinner.remove() : null;
+      }
+
+      const i = document.createElement('i')
+      i.className = "fa-brands fa-cloudscale loading input-loading";
+      formCompteur.appendChild(i)
+
+      timeout = setTimeout(() => {
+        updateCounter()
+      }, 500);
+    }
   });
-
-  function verifyToRemoveLoading() {
-
-  }
-
-  function verifyToStartLoading() {
-
-  }
 });
