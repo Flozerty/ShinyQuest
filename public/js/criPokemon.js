ada02a1664cca1d8415f775e9c1c33356bbb09ef
Flozerty <floris.louerat@gmail.com>
2024-09-02 20:22:30 +0200

    if audio running, return

--- public/js/criPokemon.js
+++ public/js/criPokemon.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const img = document.querySelector('#main-active-pkmn')
+  const img = document.querySelector('#main-active-pkmn');
+  let audio;
 
-    img.addEventListener('click', () => {
-        const pokemonId = parseInt(document.querySelector('#previous').getAttribute('data-id')) + 1
-        url = `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonId}.ogg`
-        const audio = new Audio(url);
-        audio.play();
-    })
+  img.addEventListener('click', () => {
+    if (!(audio && !audio.paused)) {
+      const pokemonId = parseInt(document.querySelector('#previous').getAttribute('data-id')) + 1;
+      const url = `https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/${pokemonId}.ogg`;
+
+      audio = new Audio(url);
+      audio.play();
+    }
+  });
 });
